8d349df987b78b0146c7aabb4cd1193b81d4114e
Kärene <143636066+Vim-Karene@users.noreply.github.com>
2025-07-01 19:06:23 +0200

    Rename RCLootCouncil to ScroogeLoot

--- Utils/tokenData.lua
+++ Utils/tokenData.lua
@@ -4,7 +4,7 @@
 -- Contains equip location and useable classes from tier tokens
 
 -- Equip locations
-RCTokenTable = {
+SLTokenTable = {
 	--[xxxxxx] = "ExampleSlot",
 
 	-- TIER 7 Naxx, OS, EoE
@@ -108,7 +108,7 @@ RCTokenTable = {
 	
 }
 
-RCTokenLevel = {
+SLTokenLevel = {
 	-- TIER 7
 	[40616] = 200,
 	[40631] = 213,
@@ -201,7 +201,7 @@ if AscensionUI then -- Ascension all tokens are used by everyone
 end
 
 -- Classes that can use the token
-RCTokenClasses = {
+SLTokenClasses = {
 	-- TIER 7
 	[40616] = CONQUEROR,
 	[40631] = CONQUEROR,
